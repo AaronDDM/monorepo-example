f06e4fe00bb0ff13c2bd8f48414893cd158f79b1
Aaron de Mello <aaron.d@nylas.com>
2023-02-18 12:02:00 -0500

    Changed shop-website build path to root dist folder

--- packages/shop-website/vite.config.ts
+++ packages/shop-website/vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(() => {
   return {
+    build: {
+      outDir: '../../dist/packages/shop-website',
+    },
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
